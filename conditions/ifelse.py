bedf42d9f06b038a192091cc80b8e53a26e6ac01
collins lenjo <collinslenjo.cl@gmail.com>
2017-05-06 23:39:33 +0300

    for loops

--- conditions/ifelse.py
+++ conditions/ifelse.py
@@ -5,3 +5,21 @@ if 5 < 2 :
 	print("My name is Bob")
 else:
 	print("Do not bite me")
+
+age = 14
+name = "Collins"
+if age > 13:
+	if name == "Collins":
+	    print("You get special priveleges")
+	else:
+		print("You are Eligible for Facebook")
+
+# Else if statements
+day = "Tuesday"
+if day == "Monday":
+	print ("Sunny!")
+elif day == "Tuesday":
+	print("Cloudy!")
+else:
+	print("Rainy!")
+
--- conditions/logical-operators.py
+++ conditions/logical-operators.py
@@ -14,3 +14,28 @@ if 5 == 5 :
 	print("My name is Bob")
 else:
 	print("Do not bite me")
+
+# Logical operators
+## Must return True or False (boolean)
+age = 14
+name = "Bob"
+if age > 13 and name == "Bob":
+	print("You are Eligible for Facebook")
+
+if age > 13 or name == "Bob":
+	print("You are Eligible for Facebook")
+
+# Loops
+## Repetiotion of code until the end of the condion gives a false boolean
+### for loop
+for i in range(0,5):
+    print(i)
+
+shopplingList = ["Milk","Bread","Eggs"]
+for i in shopplingList:
+	print(i)
+# printing even numbers
+for i in range(0,101,2):
+	print(i)
+
+###WHILE lOOPS
