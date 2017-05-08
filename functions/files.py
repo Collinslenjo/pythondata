8d7ea3972dd13d3b58635343cb3c7b7b438d2524
collins lenjo <collinslenjo.cl@gmail.com>
2017-05-08 23:00:13 +0300

    Appending

--- functions/files.py
+++ functions/files.py
@@ -1,3 +1,26 @@
 '''
 Opening and Reading files
 '''
+testfile = open("test.txt")
+file = testfile.read()
+print(file)
+# finding out where the pointer is
+position = testfile.tell()
+# reset pointer to position 0 (placing the pointer at the beggining of the file)
+position = testfile.seek(0,0)
+# Closing the file after reading.
+testfile.close()
+
+
+'''
+Appending and Reading files
+'''
+#testfile = open("test.txt","r") # for reading only
+testfile = open("test.txt","w") # for writing only (Overwrites)
+#testfile = open("test.txt","wbr") # for reading, writing and binary
+testfile.write("\n Collins is an amazing Developer\n")
+testfile.close()
+
+# Appending ( Adding without overwriting the existing content)
+testfile = open("test.txt","a") # appending
+testfile.write("\n Do what you can do with appending\n")
