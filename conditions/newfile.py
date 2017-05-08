2cfc21b6313ac7ca18390cbf2573acb66a17b595
collins lenjo <collinslenjo.cl@gmail.com>
2017-05-08 21:37:13 +0300

    filer

--- conditions/newfile.py
+++ conditions/newfile.py
@@ -3,3 +3,18 @@ Sunday Excercise
 PRIME NUMBER GENERTAOR
 print the first 10 prime numbers 
 '''
+for i in range(2,30):
+	j=2
+	counter = 0
+	while j < i:
+		if i % j == 0:
+			counter = 1
+			j = j + 1
+		else:
+			j = j + 1
+
+		if counter == 0:
+			print(str(i) +" is a prime number")
+		else:
+			counter = 0
+			print("someting random")
